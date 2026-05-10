4f945f79c6d52c02daa60f8e6d7e9b4cfa4f0441
bjustin07 <baquiranjustin07@gmail.com>
2026-05-10 09:22:12 +0800

    feature: play game now works.

--- script.js
+++ script.js
@@ -2,8 +2,11 @@
 // Get random choice from computer
 // Return random choice of computer as it's move (R/P/S)
 
-let HumanScore = 0;
-let ComputerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
+let youWinText = `You Win! Your Score: ${humanScore}, Computer Score: ${computerScore}`
+let youLoseText = `You Lose! Your Score: ${humanScore}, Computer Score: ${computerScore}`
+let drawText = `Draw!`
 
 function randomInt(range = 3){
     return Math.floor(Math.random() * range)
@@ -32,7 +35,7 @@ function getMove(moveNumber){
 
 function getHumanChoice(){
     let humanInput = parseInt(prompt("What's your move? 0: Rock, 1: Paper, 2: Scissors"))
-    while(isNaN(humanInput) || parseInt(humanInput) > 3){
+    while(isNaN(humanInput) || parseInt(humanInput) > 3 || humanInput === undefined || humanInput === null){
         if(isNaN(humanInput)){
             humanInput = prompt("Thats an invalid move! Input a number!!! 0: Rock, 1: Paper, 2: Scissors")
         }else{
@@ -43,12 +46,70 @@ function getHumanChoice(){
     return getMove(humanInput)
 }
 
-const humanMove = getHumanChoice();
-const computerMove = getComputerChoice();
+//FIX PLAY GAME!!!! STILL LOOPS AFTER WINNER IS ALREADY AT 5
+function playGame(){
+    while(noWinner){
+            playRound()
+            console.log("Moving on to the next round!")
+    }
+   if(humanScore === 5){
+        return "You win against the Computer!"
+    }else if (computerScore === 5){
+        return "HAHA LOSER."
+    }
+   
+}
 
-//Implement winning rounds for player since computer does not matter because if computer wins we just print YOU LOSE!
-function playRound(humanMove, computerMove){
+function playRound(){
+    const humanMove = getHumanChoice();
+    const computerMove = getComputerChoice();
+    let result = determineWinner(humanMove, computerMove)
+    console.log(result)
+}
+
+function determineWinner(humanMove, computerMove){
     if(humanMove === computerMove){
-        console.log("DRAW!")
+        return (drawText + `  Your Score: ${humanScore}, Computer Score: ${computerScore} `)
+    }
+    let result = ""
+    switch (humanMove){
+        case ("Rock"):
+            if(computerMove === "Scissors"){
+                humanScore++
+                return getScore(humanScore, computerScore, true);
+            }else{
+                computerScore++;
+                return getScore(humanScore, computerScore, false);
+            }
+        case ("Paper"):
+            if (computerMove === "Rock"){
+                humanScore++
+                 return getScore(humanScore, computerScore, true);
+            }else{
+                computerScore++;
+               return getScore(humanScore, computerScore, false);
+            }
+        case ("Scissors"):
+            if(computerMove === "Paper"){
+                humanScore++;
+                 return getScore(humanScore, computerScore, true);
+            }else{
+                computerScore++
+               return getScore(humanScore, computerScore, false);
+            }
+        default:
+            return console.log("Unexpected moves, please try again.")
+
     }
 }
+
+function getScore(humanScore, computerScore, gameResult){
+    if(gameResult === true){
+        return `You Win! Your Score: ${humanScore}, Computer Score: ${computerScore}`    
+    }
+    return `You Lose! Your Score: ${humanScore}, Computer Score: ${computerScore}`    
+    
+}
+
+let gameResult = playGame();
+console.log(gameResult)
